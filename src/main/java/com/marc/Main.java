5d3cb68c13fb267abc3e5ddc6622d125729486e2
Marc <marc.schoenig@gmail.com>
2021-01-25 20:07:45 +0100

    without block obj

--- src/main/java/com/marc/Main.java
+++ src/main/java/com/marc/Main.java
@@ -1,9 +1,18 @@
 package com.marc;
 
+import java.awt.Point;
+
 public class Main {
 
     public static void main(String[] args) {
-       //some preversion
+        Point p = new Point();
+        Point block = new Point(4, 25);
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 40; j++) {
+                p.setLocation(i, j);
+                System.out.print("#");
+            }
+            System.out.println();
+        }
     }
-
 }
