8c3ece7219a0aedbe83d18ef02eb536eb9739d12
Marc <marc.schoenig@gmail.com>
2021-01-25 19:55:46 +0100

    added block to map

--- src/main/java/com/marc/Main.java
+++ src/main/java/com/marc/Main.java
@@ -3,12 +3,15 @@ package com.marc;
 import java.awt.Point;
 
 public class Main {
+
     public static void main(String[] args) {
         Point p = new Point();
+        Point block = new Point(4,25);
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 40; j++) {
                 p.setLocation(i,j);
                 System.out.print("#");
+                if(p.equals(block)) System.out.print("U");
             }
             System.out.println();
         }
