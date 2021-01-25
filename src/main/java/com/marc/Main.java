ab2764ffbb2ecad0327572b68939dbb1317b1147
Marc <marc.schoenig@gmail.com>
2021-01-25 20:03:37 +0100

    added comment

--- src/main/java/com/marc/Main.java
+++ src/main/java/com/marc/Main.java
@@ -3,15 +3,9 @@ package com.marc;
 import java.awt.Point;
 
 public class Main {
+
     public static void main(String[] args) {
-        Point p = new Point();
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 40; j++) {
-                p.setLocation(i,j);
-                System.out.print("#");
-            }
-            System.out.println();
-        }
+       //some preversion
     }
 
 }
